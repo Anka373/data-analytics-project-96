3c870e8a60487d6ddff9f8e1d2288d9b680d57d7
Анна <antareska333@gmail.com>
2024-10-02 21:02:25 +0700

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -34,17 +34,61 @@ group by visit_date;
 -- каналы, приводящие пользователей (по дням)
 with tab as (
     select
-        source,
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number()
+            over (partition by s.visitor_id order by s.visit_date desc)
+        as rn
+    from sessions as s
+    left join
+        leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
+    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+),
+
+tab2 as (
+    select
+        visitor_id,
+        visit_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id
+    from tab
+    where rn = 1
+    order by
+        amount desc nulls last,
+        visit_date asc,
+        utm_source asc,
+        utm_medium asc,
+        utm_campaign asc
+),
+
+tab3 as (
+    select
+        utm_source,
         date(visit_date) as visit_date
-    from sessions
+    from tab2
 )
 
 select
     visit_date,
-    source,
-    count(source) as source_count
-from tab
-group by visit_date, source
+    utm_source,
+    count(utm_source) as source_count
+from tab3
+group by visit_date, utm_source
 order by visit_date asc, source_count desc;
 
 -- каналы, приводящие пользователей (по неделям)
