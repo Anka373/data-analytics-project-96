4916dc591db30f22020447a927a923288efe142e
Анна <antareska333@gmail.com>
2024-10-08 19:27:28 +0700

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -11,7 +11,7 @@ with tab as (
         l.status_id,
         s.source as utm_source,
         row_number()
-            over (partition by s.visitor_id order by s.visit_date desc)
+        over (partition by s.visitor_id order by s.visit_date desc)
         as rn
     from sessions as s
     left join
